aa132bedf1530095bd1aa87b68b990a16b2a34d1
NAOMI FLOR SEREBRINSKY <49627257@ort.edu.ar>
2026-03-13 11:41:23 -0300

    primera prueba

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("hola");
+
+import fs from 'fs';
+fs.writeFileSync('archivo.txt', 'Hola, este es un archivo de texto');
+console.log('Archivo creado exitosamente');
+
+const contenido = fs.readFileSync('archivo.txt', 'utf-8');
+console.log('Contenido del archivo:', contenido);
+
+
+import saludar from './saludo.js';
+const mensaje = saludar('Manuuuuu');
+console.log(mensaje);
+
+import cowsay from 'cowsay';
+
+const mensajeSegundo = cowsay.say({
+    text: 'Hola, soy una vaca que habla!',
+    e: 'oO',
+    T: 'U '
+});
+console.log(mensajeSegundo);
---
+++ saludo.js
@@ -0,0 +1,10 @@
+function saludar(nombre) {
+    return `Hola, ${nombre}! Bienvenidos a Node.js.`;
+}
+
+export default saludar;
+
+
+
+
+
